b47621b7478a45b31729735a246b519d7238a376
toru <toru@toscani>
2015-04-09 08:23:45 +0900

    Hello worldのステップ1を作成

---
+++ HelloJavaFx/src/hello/HelloJavaFx.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) by TAKAHASHI,Toru
+ */
+package hello;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author toru
+ */
+public class HelloJavaFx extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+    
+}
