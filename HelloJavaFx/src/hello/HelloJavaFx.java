acfbee76bb143f3b892a192fb1cba41692fd0c2b
toru <toru@toscani>
2015-04-09 08:34:31 +0900

    Hello worldのステップ2を作成

--- HelloJavaFx/src/hello/HelloJavaFx.java
+++ HelloJavaFx/src/hello/HelloJavaFx.java
@@ -4,6 +4,9 @@
 package hello;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +17,12 @@ public class HelloJavaFx extends Application {
 
     @Override
     public void start(Stage stage) {
+        Group root = new Group();
+        Label label = new Label("Hello, JavaFX world.");
+        root.getChildren().add(label);
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
     
