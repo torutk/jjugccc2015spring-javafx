107938a5d1506f1e7521cb4404684e19c89814f8
toru <toru@toscani>
2015-04-10 21:53:55 +0900

    アナログ時計Java API版のステップ2を作成

--- AnalogClockDrawing/src/analogclock/drawing/AnalogClockDrawing.java
+++ AnalogClockDrawing/src/analogclock/drawing/AnalogClockDrawing.java
@@ -3,9 +3,13 @@
  */
 package analogclock.drawing;
 
+import java.time.LocalTime;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.IntStream;
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -23,6 +27,7 @@ import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * アナログ時計（Javaの2DグラフィックスAPIで）
@@ -38,6 +43,14 @@ public class AnalogClockDrawing extends Application {
         Node minuteHand = createMinuteHand();
         Node secondHand = createSecondHand();
         Node centerPoint = createCenterPoint();
+        
+        LocalTime now = LocalTime.now();
+        RotateTransition secondHandTransition = createRotateTransition(Duration.seconds(60d), secondHand, getAngleOfSecond(now));
+        secondHandTransition.play();
+        RotateTransition minuteHandTransition = createRotateTransition(Duration.minutes(60d), minuteHand, getAngleOfMinute(now));
+        minuteHandTransition.play();
+        RotateTransition hourHandTransition = createRotateTransition(Duration.hours(12d), hourHand, getAngleOfHour(now));
+        hourHandTransition.play();
         
         root.getChildren().addAll(
                 clockDial, hourHand, minuteHand, secondHand, centerPoint
@@ -127,4 +140,48 @@ public class AnalogClockDrawing extends Application {
         return new Circle(100, 100, 100 * 0.05, Color.BLACK);
     }
 
+    /**
+     * 360度回転を繰り返すアニメーションを作成する。
+     * @param duration １回転に要する時間
+     * @param node 回転させるノード
+     * @param startAngle 回転開始角度（度）
+     * @return 指定したパラメータで初期化したRotateTransitionインスタンス
+     */
+    private RotateTransition createRotateTransition(
+            Duration duration, Node node, double startAngle
+    ) {
+        RotateTransition rt = new RotateTransition(duration, node);
+        rt.setFromAngle(startAngle);
+        rt.setByAngle(360d);
+        rt.setCycleCount(Animation.INDEFINITE);
+        rt.setInterpolator(Interpolator.LINEAR);
+        return rt;
+    }
+
+    /**
+     * 指定した時刻における秒針の角度（0秒を0度とし時計回りを正）を返却する。
+     * @param time 時刻
+     * @return 指定した時刻における秒針の角度
+     */
+    private double getAngleOfSecond(LocalTime time) {
+        return time.getSecond() * 360 / 60;
+    }
+
+    /**
+     * 指定した時刻における長針（分針）の角度（0分を0秒とし時計回りを正）を返却する。
+     * @param time 時刻
+     * @return 指定した時刻における長針の角度
+     */
+    private double getAngleOfMinute(LocalTime time) {
+        return (time.getMinute() + time.getSecond() / 60d) * 360 / 60;
+    }
+
+    /**
+     * 指定した時刻における短針（時針）の角度（0時/12時を0度とし時計回りを正）を返却する。
+     * @param time 時刻
+     * @return 指定した時刻における短針の角度
+     */
+    private double getAngleOfHour(LocalTime time) {
+        return (time.getHour() % 12 + time.getMinute() / 60d + time.getSecond() / (60d * 60d)) * 360 / 12;
+    }
 }
